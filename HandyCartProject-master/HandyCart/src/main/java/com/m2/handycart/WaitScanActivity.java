532dd4a835f4fd99370621a5ce22b709d58c7751
blackjmxx <alandou.jeanmichel@gmail.com>
2014-02-25 16:42:14 +0100

    Commit ihm

---
+++ HandyCartProject-master/HandyCart/src/main/java/com/m2/handycart/WaitScanActivity.java
@@ -0,0 +1,23 @@
+package com.m2.handycart;
+
+import android.support.v7.app.ActionBarActivity;
+import android.support.v7.app.ActionBar;
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.ViewGroup;
+import android.os.Build;
+
+public class WaitScanActivity extends MainActivity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wait_scan);
+    }
+
+
+}
